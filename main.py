157602d5fcf3b9f48a136b390cd4092bc7c530b8
Kye <kye@apacmediasolutions.com>
2023-10-14 20:51:44 -0400

    clean up

--- main.py
+++ main.py
@@ -1,128 +1,105 @@
 import datetime
 import logging
 import os
-import time
-
 import requests
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from undetected_chromedriver import Chrome, ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
 
-def get_cookie_value():
-    options = Options()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options)
+class Dalle:
+    """
+    A class used to interact with the DALL-E 3 Unofficial API
 
-    # Rest of your code...
-    # Rest of your code...
+    ...
 
-    # Go to Bing Image Creator and log in manually here.
-    # Once logged in, the script will proceed to get the cookie.
-    driver.get("https://www.bing.com/images/create")
+    Attributes
+    ----------
+    options : ChromeOptions
+        a ChromeOptions object to configure the Chrome driver
+    driver : Chrome
+        a Chrome driver object to interact with the website
+    cookie_value : str
+        a string representing the cookie value to bypass automation detection
 
-    input("Press Enter after you've logged in...")
+    Methods
+    -------
+    get_time():
+        Returns the current time in the format "[%d/%m/%Y %H:%M:%S]"
+    get_time_save():
+        Returns the current time in the format "%d-%m-%Y %H-%M-%S"
+    download_images(urls: list, save_folder: str):
+        Downloads images from the provided URLs and saves them in the specified folder
+    open_website(query: str):
+        Opens the Bing Image Creator (DALL-E 3) and adds a cookie
+    get_urls():
+        Extracts and returns image URLs from the website
+    """
 
-    # Open developer tools
-    ActionChains(driver).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys(
-        "j"
-    ).key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
-    time.sleep(2)  # Allow devtools to open
+    def __init__(self, cookie_value: str):
+        self.options = ChromeOptions()
+        self.options.add_argument("--disable-blink-features=AutomationControlled")
+        self.options.add_argument("--headless")
+        self.driver = Chrome(options=self.options)
+        self.cookie_value = cookie_value
 
-    # Execute JavaScript code to return the value of '_U' cookie
-    cookie_value = driver.execute_script(
-        'return document.cookie.split("; ").find(row => row.startsWith("_U=")).split("=")[1]'
-    )
-    driver.quit()
+    @staticmethod
+    def get_time():
+        """Returns the current time in the format "[%d/%m/%Y %H:%M:%S]"""
+        return datetime.datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
 
-    return cookie_value
+    @staticmethod
+    def get_time_save():
+        """Returns the current time in the format "%d-%m-%Y %H-%M-%S" """
+        return datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
 
+    def download_images(self, urls: list, save_folder: str):
+        """Downloads images from the provided URLs and saves them in the specified folder"""
+        save_folder = (save_folder)[:225]
+        try:
+            timestamp_folder = os.path.join(save_folder, self.get_time_save())
+            if not os.path.exists(timestamp_folder):
+                os.makedirs(timestamp_folder)
 
-# Function to get the current time in a specific format
-def get_time():
-    return datetime.datetime.now().strftime("[%d/%m/%Y %H:%M:%S]")
+            for index, url in enumerate(urls):
+                response = requests.get(url)
+                response.raise_for_status()
+                filename = os.path.join(timestamp_folder, f"{save_folder} ({index + 1}).png")
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
 
+                logging.info(f'{self.get_time()} Image downloaded successfully and saved to "{filename}"')
 
-# Function to get the current time in a format suitable for file and folder names
-def get_time_save():
-    return datetime.datetime.now().strftime("%d-%m-%Y %H-%M-%S")
+        except requests.exceptions.RequestException as e:
+            logging.critical(f"Image download failed: {str(e)}")
 
+    def open_website(self, query: str):
+        """Opens the Bing Image Creator (DALL-E 3) and adds a cookie"""
+        cookie = {"name": "_U", "value": self.cookie_value}
 
-# Function to download images from given URLs
-def download_images(urls, save_folder):
-    save_folder = (save_folder)[:225]
-    try:
-        timestamp_folder = os.path.join(save_folder, get_time_save())
-        if not os.path.exists(timestamp_folder):
-            os.makedirs(timestamp_folder)
+        self.driver.get(f'https://www.bing.com/images/create?q={query}')
+        logging.info(f"{self.get_time()} Bing Image Creator (Dalle-3) Opened")
 
-        for index, url in enumerate(urls):
-            response = requests.get(url)
-            response.raise_for_status()
-            filename = os.path.join(
-                timestamp_folder, f"{save_folder} ({index + 1}).png"
-            )
-            with open(filename, "wb") as file:
-                file.write(response.content)
+        self.driver.add_cookie(cookie)
+        self.driver.refresh()
+        logging.info(f"{self.get_time()} Cookie values added ")
 
-            logging.info(
-                f'{get_time()} Image downloaded successfully and saved to "{filename}"'
-            )
+        return True
 
-    except requests.exceptions.RequestException as e:
-        logging.critical(f"Image download failed: {str(e)}")
+    def get_urls(self):
+        """Extracts and returns image URLs from the website"""
+        try:
+            urls = list(set([element.get_attribute("src") for element in WebDriverWait(self.driver, 600).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "mimg")))]))
 
+            urls = [url.split('?')[0] for url in urls]
 
-# Function to open the Bing website and set cookies
-def open_website(query, cookie_value):
-    options = ChromeOptions()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--headless")
-    driver = Chrome(options=options)
-
-    cookie = {"name": "_U", "value": cookie_value}
-    driver.get(f"https://www.bing.com/images/create?q={query}")
-    logging.info(f"{get_time()} Bing Image Creator (Dalle-3) Opened")
-
-    driver.add_cookie(cookie)
-    driver.refresh()
-    logging.info(f"{get_time()} Cookie values added ")
-    return driver
-
-
-# Function to get image URLs
-def get_urls(driver):
-    try:
-        urls = list(
-            set(
-                [
-                    element.get_attribute("src")
-                    for element in WebDriverWait(driver, 600).until(
-                        EC.presence_of_all_elements_located((By.CLASS_NAME, "mimg"))
-                    )
-                ]
-            )
-        )
-
-        urls = [url.split("?")[0] for url in urls]
-        return urls
-    except Exception as e:
-        logging.critical(
-            f"Error while extracting image urls. Maybe something is wrong about your prompt. (You can check your prompt manually) \n{e}"
-        )
-
-
-# Main part of the script
+            return urls
+        except Exception as e:
+            logging.critical(
+                f"Error while extracting image urls. Maybe something is wrong about your prompt. (You can check you prompt manually) \n{e}")
+            
 if __name__ == "__main__":
     query = "Military Byzantine Orthodox army legions world domination, holy orthodox christian warriors, concept"
-    save_folder = "images/"
-    cookie_value = get_cookie_value()
-    driver = open_website(query, cookie_value)
-    urls = get_urls(driver)
-    download_images(urls, save_folder)
+    api = Dalle(query)
+    api.run()
