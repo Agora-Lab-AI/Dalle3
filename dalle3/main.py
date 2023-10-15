4ea8378aeb830a66447d7611f4e41b78b6d4755f
Kye <kye@apacmediasolutions.com>
2023-10-14 22:06:58 -0400

    docs

--- dalle3/main.py
+++ dalle3/main.py
@@ -35,6 +35,30 @@ class Dalle3:
         Opens the Bing Image Creator (DALL-E 3) and adds a cookie
     get_urls():
         Extracts and returns image URLs from the website
+
+
+    Usage:
+    ------
+    # Import the necessary module
+    import logging
+    from dalle3 import Dalle3
+
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Instantiate the Dalle3 class with your cookie value
+    dalle = Dalle3("")
+
+    # Open the website with your query
+    dalle.open_website(
+        "Fish hivemind swarm in light blue avatar anime in zen garden pond concept art anime art, happy fish, anime scenery"
+    )
+
+    # Get the image URLs
+    urls = dalle.get_urls()
+
+    # Download the images to your specified folder
+    dalle.download_images(urls, "images/")
     """
 
     def __init__(self, cookie_value: str):
