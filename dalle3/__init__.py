b07e7887aed9647492e8227f26c6ac5ba411f775
Kye <kye@apacmediasolutions.com>
2023-10-19 13:22:01 -0400

    clean up

--- dalle3/__init__.py
+++ dalle3/__init__.py
@@ -1,4 +1,4 @@
 from dalle3.dalle import Dalle
-from dalle3.idea2image import Idea2Image
+# from dalle3.idea2image import Idea2Image
 
-__all__ = ["Dalle", "Idea2Image"]
+__all__ = ["Dalle"]  #"Idea2Image"]
